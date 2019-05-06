f8fd84669db2f2148c367fd90202a9b932af53c1
js-shane <js-shane@tl.com>
2019-05-06 11:19:14 +0700

    统一IDE文本编辑器的行为

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        home: './src/pages/home/index.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/pages/home/index.html",
-        }),
-    ],
+  entry: {
+    home: './src/pages/home/index.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/pages/home/index.html",
+    }),
+  ],
 };
