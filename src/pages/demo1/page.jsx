734c806b91ca72582896f7c314435f5476956f9c
js-shane <js-shane@tl.com>
2019-08-06 22:23:43 +0700

    add 添加单页应用demo

--- src/pages/demo1/page.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import FormContainer from './components/container/FormContainer.jsx'
-
-const wrapper = document.getElementById('app')
-wrapper && ReactDOM.render(<FormContainer />, wrapper)
