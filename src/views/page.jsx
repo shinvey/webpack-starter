023caaacc12ce51fa41a2920ad3b6b4069d45460
js-shane <js-shane@tl.com>
2019-08-09 22:00:12 +0700

    启用react hot loader

--- src/views/page.jsx
+++ src/views/page.jsx
@@ -2,5 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import Container from './Container.jsx'
 
+// 启用react hot loader https://github.com/gaearon/react-hot-loader
+import { hot } from 'react-hot-loader/root'
+const App = hot(() => <Container />)
+
 const wrapper = document.getElementById('app')
-wrapper && render(<Container />, wrapper)
+wrapper && render(<App />, wrapper)
