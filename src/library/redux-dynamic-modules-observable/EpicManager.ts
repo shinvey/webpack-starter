3f04bc028ba5e31e2fd6fb6fca13bc543989d582
js-shane <js-shane@tl.com>
2019-08-30 21:07:48 +0700

    upd 添加epic动态加载用例

--- src/library/redux-dynamic-modules-observable/EpicManager.ts
+++ src/library/redux-dynamic-modules-observable/EpicManager.ts
@@ -1,71 +1,158 @@
-import { getStringRefCounter, IItemManager } from "redux-dynamic-modules";
+/* tslint:disable */
+
+import { getStringRefCounter, getObjectRefCounter, IItemManager } from "redux-dynamic-modules-core";
 import { Epic } from "redux-observable";
-import { merge, BehaviorSubject, of } from "rxjs";
+import { merge, BehaviorSubject, of, Subject } from "rxjs";
 import { mergeMap, switchMap, mapTo } from 'rxjs/operators'
 import { combineEpics, ofType } from 'redux-observable';
 
+// import { Subject, from, queueScheduler } from 'rxjs';
+// const QueueScheduler = queueScheduler.constructor;
+// const uniqueQueueScheduler = new QueueScheduler(queueScheduler.SchedulerAction);
+
 export interface IEpicManager extends IItemManager<Epic> {
-  rootEpic: Epic;
+  // rootEpic: Epic;
 }
 
 /**
  * Creates an epic manager which manages epics being run in the system
  */
-export function getEpicManager(): IEpicManager {
-  let runningEpics: Epic[] = [];
-  const epicRefCounter = getStringRefCounter();
+export function getEpicManager(epicMiddleware): IEpicManager {
+  let runningEpics: Object = {};
+  // @ts-ignore
+  const epicRefCounter = getObjectRefCounter();
+  const has = (key) => key in runningEpics;
+  const get = (key) => runningEpics[key];
+  const key = (namespace, name) => `${namespace}-${name}`
+  // const rootEpic: Epic = createRootEpic(runningEpics);
 
-  const rootEpic: Epic = createRootEpic(runningEpics);
-
-  return {
-    getItems: (): Epic[] => runningEpics,
-    rootEpic,
+  return <IEpicManager>{
+    // @ts-ignore
+    getItems: () => runningEpics,
+    // rootEpic,
     /**
      * 生产版本 epic只能加一次，必须做放重复检查
      * 开发版本可以尝试考虑switchMap来替换已存在的epic
-     * @param epics
+     *
+     * todo 避免重复添加epic，可以使用对象key 加 module id
+     * todo 实现替换epic
+     *
      */
-    add: (epics: Epic[]) => {
+    add: (epics: Epic[], namespace) => {
       if (!epics) {
         return;
       }
 
-      epics.forEach(e => {
-        rootEpic.replace(e);
-        epicRefCounter.add(e.name);
-        runningEpics.push(e);
-      });
+      let arrEpicWrapper = runningEpics[namespace] || [];
+
+      epics.forEach((epic: Epic, index) => {
+        // const epicKey = key(namespace, epic.name)
+        // console.debug('check ', epicKey, ' if it existed ', has(epicKey))
+
+        // 发现已经被引用了
+        // if (epicRefCounter.getCount(epic)) {
+        //   // 如果key不一样，更新key
+        //   epicKey ===
+        //   // do nothing
+        // }
+
+        /**
+         * 考虑hot reload场景
+         * 当名称一致，函数引用不一致时可以考虑替换
+         * fixme 如何处理函数逻辑一致，key更改的情况
+         */
+        if (arrEpicWrapper[index]) {
+          let epicWrapper = arrEpicWrapper[index]
+          // 如果开启了HMR，尝试替换epic
+          // @ts-ignore
+          if (module.hot && epicWrapper.epicRef() != epic) {
+            // 更新引用，是为了下一次的判断或对比
+            epicRefCounter.remove(epicWrapper.epicRef())
+            epicRefCounter.add(epic)
+            /**
+             * 使用旧epic替换为新epic
+             * 不直接替换是因为rxjs本身设计模式，无法卸载已经存在的epic逻辑
+             * replaceWith会采用switchMap，来尝试替换旧epic逻辑
+             */
+            epicWrapper.replaceWith(epic)
+
+
+            console.info(`[HRM] previous epic ${epicWrapper} is replaced by new epic ${epic}`)
+            epicWrapper = undefined
+          }
+        } else {
+          const replaceableWrapper = createReplaceableWrapper()
+          epicMiddleware.run(replaceableWrapper)
+          // 让epic生效
+          replaceableWrapper.replaceWith(epic)
+          // 储存epic引用，供下次检查
+          arrEpicWrapper[index] = replaceableWrapper
+          // 引用计数, 供下次检查
+          epicRefCounter.add(epic)
+        }
+      })
+      // epicMiddleware.run(combineEpics(...replaceableWrappers));
+      // epics.forEach((e, idx) => replaceableWrappers[idx].replaceWith(e))
+
+      // epics.forEach(e => {
+      //   // @ts-ignore
+      //   // rootEpic.replace(e);
+      //   epicRefCounter.add(e.name);
+      //   runningEpics.push(e);
+      // });
     },
     remove: (epics: Epic[]) => {
       if (!epics) {
-        return;
+        return
       }
 
-      const epicNameMap = epics.reduce((p, e) => {
-        p[e.name] = e;
-        return p;
-      }, {});
+      // const epicNameMap = epics.reduce((p, e) => {
+      //   p[e.name] = e;
+      //   return p;
+      // }, {});
 
-      epics.forEach(e => {
-        epicRefCounter.remove(e.name);
-      });
+      // fixme 目前还没有真正可以移除epic的有效方法，原因请见 https://redux-observable.js.org/docs/recipes/AddingNewEpicsAsynchronously.html
+      // epics.forEach(epic => {
+      //   epicRefCounter.remove(epic);
+      // });
 
-      runningEpics = runningEpics.filter(e => {
-        !!epicNameMap[e.name] && epicRefCounter.getCount(e.name) !== 0;
-      });
+      // runningEpics = runningEpics.filter(e => {
+      //   !!epicNameMap[e.name] && epicRefCounter.getCount(e.name) !== 0;
+      // });
     },
     dispose: () => {
-      runningEpics = null;
+      runningEpics = null
     },
   };
 }
 
+/**
+ * create a wrapper which can be replace by a epic
+ * @returns replaceableWrapper
+ */
+function createReplaceableWrapper () {
+  const epic$ = new Subject();
+
+  // 包装一个可以被替换的wrapper
+  const replaceableWrapper = (...args) =>
+    epic$.pipe(
+      // @ts-ignore
+      switchMap(epic => epic(...args))
+    );
+
+  // 暴露替换方法
+  replaceableWrapper.replaceWith = epic => {
+    epic$.next(epic);
+    replaceableWrapper._epic = epic;
+  }
+  // 提供epic ref访问接口
+  replaceableWrapper.epicRef = () => replaceableWrapper._epic
+
+  return replaceableWrapper;
+}
+
 function createRootEpic(runningEpics: Epic[]): Epic {
-  const emptyEpic = action$ => action$.pipe(
-    ofType('nothing'),
-    mapTo({ type: 'nothing' })
-  )
-  const epic$ = new BehaviorSubject(emptyEpic)
+  const epic$ = new Subject();
 
   const merger = (...args) =>
     epic$.pipe(
--- src/library/redux-dynamic-modules-observable/index.ts
+++ src/library/redux-dynamic-modules-observable/index.ts
@@ -5,16 +5,16 @@ import { IEpicModule } from "./Contracts";
 
 export function getObservableExtension(): IExtension {
     const epicMiddleware = createEpicMiddleware();
-    const epicManager = getEpicManager();
+    const epicManager = getEpicManager(epicMiddleware);
 
     return {
         middleware: [epicMiddleware],
         onModuleManagerCreated: () => {
-            epicMiddleware.run(epicManager.rootEpic);
+            // epicMiddleware.run(epicManager.rootEpic);
         },
         onModuleAdded: (module: IEpicModule<any>) => {
             if (module.epics) {
-                epicManager.add(module.epics);
+                epicManager.add(module.epics, module.id);
             }
         },
         onModuleRemoved: (module: IEpicModule<any>) => {
