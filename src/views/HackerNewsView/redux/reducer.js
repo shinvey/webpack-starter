294be65910e1da88f1b6947753742ad6635dc2ac
shinvey <shinvey@qq.com>
2019-11-21 14:48:53 +0700

    UPD 使用Immer Curried producers修改reducer

--- src/views/HackerNewsView/redux/reducer.js
+++ src/views/HackerNewsView/redux/reducer.js
@@ -1,16 +1,18 @@
 import produce from 'immer'
 import { HackerNewsStoriesAvailable } from './actions'
 
-export const hackerNewsReducer = (state, action) => {
-  return produce(state || { items: [] }, draft => {
-    switch (action.type) {
-      case HackerNewsStoriesAvailable: {
-        draft.items = action.payload.items
-        break
-      }
-      default: {
-        // do nothing
-      }
+/**
+ * 使用immer的produce包装reducer需要了解的相关知识点
+ * Curried producers
+ * https://immerjs.github.io/immer/docs/curried-produce
+ * Returning new data from producers
+ * https://immerjs.github.io/immer/docs/return
+ */
+export const hackerNewsReducer = produce((draft, action) => {
+  switch (action.type) {
+    case HackerNewsStoriesAvailable: {
+      draft.items = action.payload.items
+      break
     }
-  })
-}
+  }
+}, { items: [] })
