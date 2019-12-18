53fbd4e0fc7d74b883a8424b5fde432f19d23250
shinvey <shinvey@qq.com>
2019-12-18 14:01:53 +0700

    UPD Hacker News Demo，绕过底层detectError逻辑，使其能够正常展示数据

--- src/views/HackerNewsView/redux/actions.js
+++ src/views/HackerNewsView/redux/actions.js
@@ -43,8 +43,14 @@ const fetchJson = url => {
   // get(url).then(response => { console.log('fetchJSON', response) })
   //   .catch(err => { console.error('fetchJSON', err) })
   // return get(url, { name: 'Alice' }).then(AjaxResponse => AjaxResponse.response)
-  const ajaxP = get(url)
-  const result = ajaxP.then(AjaxResponse => AjaxResponse.response)
+  const result = get(url, undefined, {
+    /**
+     * 因为是请求外部url，接口返回数据结构标准不一
+     * 为了绕过底层接口异常检测逻辑，需要给这个ajax实例修改底层默认的detectError行为
+     * 只做展示用途，平常业务开发中几乎没有这种用例
+     */
+    detectError () {}
+  })
   result.catch(err => {
     console.error('ajax error', err)
     isCancel(err) && console.log(err.message)
