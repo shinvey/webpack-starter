99019d1dde553412cc335bda98ae3011145bac55
shinvey <shinvey@qq.com>
2020-01-03 01:02:23 +0700

    UPD upgrade dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -18,7 +18,7 @@ module.exports = ({ options }) => {
        * LostGrid makes use of calc() to create stunning grids based on fractions you define without having to pass a lot of options.
        * see https://github.com/peterramsing/lost
        */
-      lost: {},
+      // lost: {},
       // https://github.com/csstools/postcss-preset-env
       // 将默认使用browsers list配置文件来断言需要支持的设备列表
       'postcss-preset-env': {
